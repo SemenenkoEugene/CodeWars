372de28e75c5575106bc79d77a8b07e1444d5525
SemenenkoEugene <gks-08@mail.ru>
2023-09-07 11:23:34 +0300

    Revert "Init"
    This reverts commit c154f40a6eef696b02b75a454010cbb0de72c857.

--- CodeWars/src/kyu_8/FIXME_Replace_all_dots.java
+++
@@ -1,9 +0,0 @@
-package kyu_8;
-/*
-Предоставленный код должен заменить все точки . в указанной строке str тире -
- */
-public class FIXME_Replace_all_dots {
-    public static String replaceDots(final String str) {
-        return str.replaceAll("\\.", "-");
-    }
-}
--- catsgram/src/main/java/ru/yandex/practicum/catsgram/controller/SimpleController.java
+++ catsgram/src/main/java/ru/yandex/practicum/catsgram/controller/SimpleController.java
@@ -1,20 +1,14 @@
-package ru.yandex.practicum.catsgram.controller;
+package ru.yandex.practicum.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 
 @RestController
 public class SimpleController {
 
-    private static final Logger log = LoggerFactory.getLogger(SimpleController.class);
-
-    @GetMapping("/home")
-    public String homePage() {
-
-        log.info("Получен запрос");
+    @RequestMapping("/home")
+    public String homePage(){
         return "Котограм";
     }
 }
