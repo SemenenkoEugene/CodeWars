85fb1578463ddce6b016e412702ec812fe3bea43
SemenenkoEugene <gks-08@mail.ru>
2023-10-20 20:10:55 +0300

    Init

---
+++ CodeWars/src/main/java/org/example/kyu_8/OppositeNumber.java
@@ -0,0 +1,17 @@
+package org.example.kyu_8;
+
+/*
+Очень просто, учитывая целое число или число с плавающей запятой, найдите его противоположность.
+
+Примеры:
+
+1: -1
+14: -14
+-34: 34
+ */
+public class OppositeNumber {
+
+    public static int opposite(int number) {
+        return number * -1;
+    }
+}
