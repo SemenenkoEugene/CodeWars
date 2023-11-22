6c15a5186937555a408a9cd250ec2c40eef19969
SemenenkoEugene <gks-08@mail.ru>
2023-11-22 21:53:44 +0300

    Init

--- CodeWars/src/main/java/org/example/kyu_8/IsDivisible.java
+++ CodeWars/src/main/java/org/example/kyu_8/IsDivisible.java
@@ -4,4 +4,13 @@ public class IsDivisible {
     public static boolean isDivisible(long n, long x, long y) {
         return n % x == 0 && n % y == 0;
     }
+
+    public static boolean isDivisible(int n, int... args) {
+        for (int arg : args) {
+            if (n % arg != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
